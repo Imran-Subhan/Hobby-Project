3cd965f7aead7d1769011f02ede77e73ddaa6445
Imran-Subhan <isubhan@qa.com>
2020-08-09 23:50:08 +0100

    HP-4 #done

--- Arsenal_Project/CreateTeam.js
+++ Arsenal_Project/CreateTeam.js
@@ -1,41 +1,48 @@
-function ShowTeam() {
-    document.getElementById("tbody").innerHTML="";
-    fetch()
-    .then(
-    function(response) {
-    if (response.status !== 200) {
-    console.log('Looks like there was a problem. Status Code: ' +
-    response.status);
-    return;
-    }
-    response.json().then(function(data) {
-        console.log(data);
-        for (let i = 0; i < 5; i++) {
-        console.log(data.results[i].playername);
-        console.log(data.results[i].playerno);
-        console.log(data.results[i].Attack);
-        console.log(data.results[i].Defence);
+window.onload(getAll());
 
-        let para = document.createElement("P");
-        let tbody = document.getElementById("tbody") // Create a <p> element
-        para;
-        tbody;
-        console.log(tbody);
-        let string = '<tr><td>'+data.results[i].name+'</td><td>'+data.results[i].url+'</tr>';
-       
-        document.getElementById("tbody").innerHTML+=string;
-        // para.className = "alert alert-success col-md-8";
-        // para.innerText = `The name is : ${(data.results[i].name)} \n
-        // The url is : ${data.results[i].url}`; // Insert text
 
-      
+function getAll() {
+    fetch('http://localhost:9001/ShowPlayers')
+      .then(
+      function(response) {
+      if (response.status !== 200) {
+      console.log('Looks like there was a problem. Status Code: ' +
+      response.status);
+      return;
+      }
+      response.json().then(function(data) {
+          console.log(data);
+          for (let i =0; i<data.length; i++) {
+            console.log(data[i].playername);
+            console.log(data[i].playerno);
+            console.log(data[i].attack);
+            console.log(data[i].defence);
+          
+          
+          let string = '<tr><td>'+data[i].playername+'</td><td>'+data[i].playerno+'</td><td>'+data[i].attack+'</td><td>' 
+          + data[i].defence+'</td><td>' + '</td><td><button onclick = "DeletePlayer('+ data[i].playerno+')"class="btn btn-block btn-danger">Delete</button></td></tr>';
+         
+          document.getElementById("tbody").innerHTML+=string;
+        
+          }
+          });
+          })
         }
-        });
-        })
 
-
-
-    .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-    });
-}
+        function DeletePlayer(id){
+            fetch('http://localhost:9001/DeletePlayer/'+id, {
+              method: 'Delete',
+              headers: {
+                "Content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+              },
+              body: 'foo=bar&lorem=ipsum'
+            })
+            .then(id)
+            .then(function (data) {
+              console.log('Request succeeded with JSON response', data);
+              location.reload();
+            })
+            .catch(function (error) {
+              console.log('Request failed', error);
+            });
+        }
--- NewProject1/src/main/java/com/qa/Controllers/PController.java
+++ NewProject1/src/main/java/com/qa/Controllers/PController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,8 +38,8 @@ public class PController {
 		return service.UpdatePlayer(id);
 		
 	}
-	@DeleteMapping ("/DeletePlayer")
-	public String deletePlayer(@RequestBody int id) {
+	@DeleteMapping ("/DeletePlayer/{id}")
+	public String deletePlayer(@PathVariable int id) {
 		return service.DeletePlayer(id);
 }
 	
