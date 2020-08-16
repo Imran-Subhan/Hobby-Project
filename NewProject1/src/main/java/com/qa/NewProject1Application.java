952f871644c98684a8123e1aa35f76c74e128c3b
Imran-Subhan <isubhan@qa.com>
2020-08-16 18:38:47 +0100

    testf

--- NewProject1/src/main/java/com/qa/NewProject1Application.java
+++ NewProject1/src/main/java/com/qa/NewProject1Application.java
@@ -1,13 +1,13 @@
-package com.qa;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class NewProject1Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(NewProject1Application.class, args);
-	}
-
-}
+//package com.qa;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class NewProject1Application {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(NewProject1Application.class, args);
+//	}
+//
+//}
