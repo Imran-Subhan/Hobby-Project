69ef7a3ced6101e6134dd859cd0bbd4390dc537b
Imran-Subhan <isubhan@qa.com>
2020-08-16 18:41:35 +0100

    testf

--- NewProject1/src/test/java/com/qa/NewProject1ApplicationTests.java
+++ NewProject1/src/test/java/com/qa/NewProject1ApplicationTests.java
@@ -1,16 +1,16 @@
-package com.qa;
-
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-class NewProject1ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.qa;
+//
+//import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//class NewProject1ApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
