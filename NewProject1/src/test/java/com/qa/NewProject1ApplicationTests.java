e847c80c640ef5ce232f45eb1e18c241a528ba3b
Imran-Subhan <isubhan@qa.com>
2020-08-16 22:16:56 +0100

    tests

--- NewProject1/src/test/java/com/qa/NewProject1ApplicationTests.java
+++ NewProject1/src/test/java/com/qa/NewProject1ApplicationTests.java
@@ -1,16 +1,46 @@
-//package com.qa;
-//
-//import org.junit.jupiter.api.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//class NewProject1ApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.qa;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.qa.Entities.Player;
+import com.qa.Repositories.PlayerRepo;
+import com.qa.Services.PlayerService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class NewProject1ApplicationTests {
+
+	@Autowired
+	private PlayerService service;
+	
+	@MockBean
+	private PlayerRepo repo;
+	
+	@Test
+	public void ShowallPlayers() {
+		when(repo.findAll()).thenReturn(Stream
+				.of(new Player(10, "Ozil", 70, 70), new Player(7, "Saka", 75, 60)).collect(Collectors.toList()));
+		assertEquals(2, service.ShowallPlayers().size());
+	}
+	
+	@Test
+	public void CreateTest() {
+		Player player = new Player(10, "Ozil", 70, 70);
+		when(repo.save(player)).thenReturn(player);
+		assertEquals(player, service.create(player));
+	}
+
+
+}
